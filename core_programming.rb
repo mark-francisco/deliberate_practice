32faef85b37fa651cb0e986b3a80d0b322d195a6
Mark Francisco <mfrancisco581@gmail.com>
2021-06-10 18:21:51 -0500

    start on delib practice .reduce stuff

--- core_programming.rb
+++ core_programming.rb
@@ -459,3 +459,75 @@
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/b9ac4390aad2301a2238efc95c904f3d
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/66598fd7cdbc67fe663624e217cebbaf
 # SOLUTIONS (using .map shortcut): https://gist.github.com/peterxjang/23a8f8a51601e4288ba3a8aa6d1f1c98
+
+###########################################################################
+###########################################################################
+
+# #  1. Start with an array of numbers and compute the sum of all the numbers.
+# #     For example, [5, 10, 8, 3] becomes 26.
+# def sum_of_all_numbers(arr)
+#   sum = 0
+#   i = 0
+#   while i < arr.length
+#     sum += arr[i]
+#     i += 1
+#   end
+#   return sum
+# end
+
+# pp sum_of_all_numbers([5, 10, 8, 3])
+
+# #  2. Start with an array of strings and combine them all into a single string.
+# #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+# def concat_strings(arr)
+#   result = ""
+#   arr.each { |arr_item|
+#     result += arr_item.to_s
+#   }
+#   return result
+# end
+
+# pp concat_strings(["volleyball", "basketball", "badminton"])
+
+# #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+# #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+# def sum_of_price_key(arr)
+#   return arr.reduce(0) { |sum, arr_item|
+#            sum + arr_item[:price]
+#          }
+# end
+# pp sum_of_price_key([{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }])
+
+#  4. Start with an array of numbers and compute the the minumum number.
+#     For example, [5, 10, 8, 3, 9] becomes 3.
+# def arr.reduce(arr[0]) { |min, arr_item|
+#   if arr_item <= min
+#     min = arr_item
+#   end
+#   return min
+# } 
+
+puts "you left off here"
+
+end
+#  5. Start with an array of strings and compute the total length of all the strings.
+#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+#  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+#  7. Start with an array of numbers and compute product of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 1200.
+
+#  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+#  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+# 10. Start with an array of numbers and compute the maximum number.
+#     For example, [5, 10, 8, 3] becomes 10.
+
+# SOLUTIONS (using while loop): https://gist.github.com/peterxjang/376c8931a48549889eb3c9bc091e9f43
+# SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/379c9945774f51027750c59d6e4395df
+# SOLUTIONS (using .reduce shortcut): https://gist.github.com/peterxjang/b69183e2d555964ce3936893f423ef35
